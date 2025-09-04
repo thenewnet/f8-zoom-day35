6ee8df906d258c42f9203cd0b102123585a677d1
trungbq <trungbq86@gmail.com>
2025-09-05 03:01:58 +0700

    buttons

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,13 +1,56 @@
 import PropTypes from 'prop-types';
+import clsx from 'clsx';
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+
 import styles from './Button.module.scss';
 
-function Button({ title, children }){
-    return <button>{children}</button>
+
+function Button({
+    primary = false,
+    bordered = false,
+    rounded = false,
+    size = 'medium',
+    href,
+    children,
+    className,
+    disabled,
+    loading,
+    ...passProps
+}) {
+
+    const classNames = clsx(styles.wrapper, styles[size], className,
+        {
+            [styles.primary]: primary,
+            [styles.bordered]: bordered,
+            [styles.rounded]: rounded,
+            [styles.disabled]: disabled,
+            [styles.loading]: loading,
+        });
+
+    const Component = href ? 'a' : 'button';
+
+    return <Component {...passProps} href={href} className={classNames}>
+        {children} 
+        {loading && (
+            <span className={styles.loadingIcon}>
+                <FontAwesomeIcon icon={faSpinner} spin />
+            </span>
+        )}
+    </Component>
 }
 
 Button.PropTypes = {
-    title: PropTypes.string,
+    primary: PropTypes.bool,
+    bordered: PropTypes.bool,
+    rounded: PropTypes.bool,
+    size: PropTypes.string,
+    href: PropTypes.string,
     children: PropTypes.node.isRequired,
+    className: PropTypes.string,
+    disabled: PropTypes.bool,
+    loading: PropTypes.bool,
 
 }
 export default Button;
--- src/pages/Button/index.jsx
+++ src/pages/Button/index.jsx
@@ -1,7 +1,72 @@
+import styles from './Button.module.scss';
+import Button from '../../components/Button';
+
 function Buttons() {
     return (
         <>
-            <h1>Buttons</h1>
+            <h1 className={styles.page_title}>Buttons Demo</h1>
+            <div className={styles.wrapper}>
+                <div>
+                    Basic Button: <Button>Click me</Button>
+                </div>
+
+                <div>
+                    Primary Button: <Button primary>Primary Button</Button>
+                </div>
+
+                <div>
+                    Link Button: <Button href="https://google.com" target="_blank">
+                        Go to Google
+                    </Button>
+                </div>
+                <div>
+                    Button với Size:
+                    <Button size="small">Small</Button>
+                    <Button size="medium">Medium</Button>
+                    <Button size="large">Large</Button>
+                </div>
+                <div>
+                    Button với Variants:
+                    <Button bordered>Bordered</Button>
+                    <Button rounded>Rounded</Button>
+                    <Button primary rounded>Primary Rounded</Button>
+                </div>
+                <div>
+                    Button với onClick:
+                    <Button onClick={() => alert('Clicked!')}>
+                        Click Alert
+                    </Button>
+                </div>
+                <div>
+                    Disabled Button:
+                    <Button disabled onClick={() => alert('Should not show')}>
+                        Disabled Button
+                    </Button>
+                </div>
+                <div>
+                    Loading Button:
+                    <Button loading onClick={() => console.log('Should not log')}>
+                        Loading Button
+                    </Button>
+                    <Button loading bordered onClick={() => console.log('Should not log')}>
+                        Loading Button
+                    </Button>
+                </div>
+                <div>
+                    Custom className:
+                    <Button className={styles.my_custom_class} primary>
+                        Custom Styled
+                    </Button>
+                </div>
+                <div>
+                    Button với Icon:
+                    <Button primary>
+                        <span>📧</span> Send Email
+                    </Button>
+                </div>
+            </div>
+
+
         </>
     )
 }
